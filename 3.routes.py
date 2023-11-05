e7e18e9103d599967334de6a047c0d167e026219
nehanare <116936069+nehanare@users.noreply.github.com>
2023-11-05 13:20:11 +0530

    Create 3.routes.py

---
+++ 3.routes.py
@@ -0,0 +1,27 @@
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/transcript', methods=['POST'])
+def transcript():
+    video_url = request.form['video_url']
+    try:
+        # download the video
+        yt = YouTube(video_url)
+        video_path = yt.streams.get_audio_only().download()
+
+        # extract the transcript
+        transcript_list = YouTubeTranscriptApi.get_transcript(yt.video_id)
+
+        # format the transcript with timestamps
+        transcript = []
+        for line in transcript_list:
+            timestamp = line['start']
+            text = line['text']
+            transcript.append({'timestamp': timestamp, 'text': text})
+      
+        # display the transcript
+        return render_template('transcript.html', transcript=transcript)
+      except:
+
+        return render_template('error.html')
