abdc7e6eb5a5340ec0a63a46ef685c9d71d516fc
nehanare <116936069+nehanare@users.noreply.github.com>
2023-11-05 13:25:34 +0530

    Create 5.testing.py

---
+++ 5.testing.py
@@ -0,0 +1,5 @@
+python app.py
+
+# output seen should be:
+# Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+# Enter a valid YouTube video URL in the input field on the home page and click the “Generate Transcript” button. If the video URL is valid, the transcript should be displayed on the next page with timestamps for each line. If an error occurs, the error page should be displayed.
